a68e687482b116f219a44a704cf9d75821d22f0d
Tawhid03 <tawhidahmed2003@gmail.com>
2024-12-06 21:30:56 -0500

    Update package.json with necessary changes

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,6 @@
 "use client";
 
-import { Authenticated, Unauthenticated } from "convex/react";
+import { AuthLoading, Authenticated, Unauthenticated } from "convex/react";
 import { SignInButton, UserButton } from "@clerk/nextjs";
 import { NewToDoForm } from "./_component/new-todo-form";
 import ToDoList from "./_component/to-do-list";
@@ -8,7 +8,7 @@ import ToDoList from "./_component/to-do-list";
 export default function Home() {
   return (
     <div className="max-w-screen-md mx-auto p-4 space-y-4">
-      <h1 className="text-xl font-bold">To-Do List</h1>
+      <h1 className="text-xl font-bold"></h1>
 
       {/* Authenticated View */}
       <Authenticated>
@@ -22,10 +22,13 @@ export default function Home() {
       {/* Unauthenticated View */}
       <Unauthenticated>
         <div className="text-center space-y-4">
-          <p className="text-gray-700">Please sign in to manage your tasks.</p>
+          <p className="text-gray-700">Please sign in to continue</p>
           <SignInButton />
         </div>
       </Unauthenticated>
+      <AuthLoading>
+        loading...
+      </AuthLoading>
     </div>
   );
 }
