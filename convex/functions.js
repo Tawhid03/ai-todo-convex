a817e0b0ce06694d824cf2fe65ca6c694e2bbd21
Tawhid03 <tawhidahmed2003@gmail.com>
2024-12-09 18:12:35 -0500

    Done

--- convex/functions.js
+++ convex/functions.js
@@ -1,49 +1,31 @@
 // File: convex/functions.js
 
-import { queryGeneric, mutationGeneric } from "convex/server";
+import { query, mutation } from "convex/server";
 import { v } from "convex/values";
 
 // Query to fetch all todos
-export const getTodos = queryGeneric({
-  handler: async (ctx) => {
-    return await ctx.db.query("todos").collect();
-  },
+export const getTodos = query(async ({ db }) => {
+  return await db.table("todos").collect();
 });
 
 // Mutation to add a new todo
-export const addTodo = mutationGeneric({
-  args: {
-    title: v.string(),
-    description: v.string(),
-  },
-  handler: async (ctx, args) => {
-    const todo = {
-      title: args.title,
-      description: args.description,
-      completed: false,
-    };
-    await ctx.db.insert("todos", todo);
-  },
+export const addTodo = mutation(async ({ db }, { title, description }) => {
+  const todo = {
+    title,
+    description,
+    completed: false,
+  };
+  await db.table("todos").insert(todo);
 });
 
 // Mutation to toggle a todo's completed state
-export const toggleTodo = mutationGeneric({
-  args: {
-    id: v.id("todos"),
-  },
-  handler: async (ctx, args) => {
-    const todo = await ctx.db.get(args.id);
-    if (!todo) throw new Error("Todo not found");
-    await ctx.db.patch(args.id, { completed: !todo.completed });
-  },
+export const toggleTodo = mutation(async ({ db }, { id }) => {
+  const todo = await db.table("todos").get(id);
+  if (!todo) throw new Error("Todo not found");
+  await db.table("todos").update(id, { completed: !todo.completed });
 });
 
 // Mutation to delete a todo
-export const deleteTodo = mutationGeneric({
-  args: {
-    id: v.id("todos"),
-  },
-  handler: async (ctx, args) => {
-    await ctx.db.delete(args.id);
-  },
+export const deleteTodo = mutation(async ({ db }, { id }) => {
+  await db.table("todos").delete(id);
 });
