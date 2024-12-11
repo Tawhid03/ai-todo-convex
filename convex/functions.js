d8077b08f0866131906bea97afe1b4872d93b6b6
Tawhid03 <tawhidahmed2003@gmail.com>
2024-12-11 12:43:56 -0500

    convex update

--- convex/functions.js
+++ convex/functions.js
@@ -1,31 +1,29 @@
 // File: convex/functions.js
-
 import { query, mutation } from "convex/server";
 import { v } from "convex/values";
 
 // Query to fetch all todos
 export const getTodos = query(async ({ db }) => {
-  return await db.table("todos").collect();
+  return await db.query("todos").collect();
 });
 
 // Mutation to add a new todo
 export const addTodo = mutation(async ({ db }, { title, description }) => {
-  const todo = {
+  await db.insert("todos", {
     title,
     description,
     completed: false,
-  };
-  await db.table("todos").insert(todo);
+  });
 });
 
 // Mutation to toggle a todo's completed state
 export const toggleTodo = mutation(async ({ db }, { id }) => {
-  const todo = await db.table("todos").get(id);
+  const todo = await db.get(id);
   if (!todo) throw new Error("Todo not found");
-  await db.table("todos").update(id, { completed: !todo.completed });
+  await db.patch(id, { completed: !todo.completed });
 });
 
 // Mutation to delete a todo
 export const deleteTodo = mutation(async ({ db }, { id }) => {
-  await db.table("todos").delete(id);
+  await db.delete(id);
 });
